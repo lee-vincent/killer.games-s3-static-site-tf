801061b58412ba14691d6d5ebecef20e52acbc3c
lee-vincent <vinnie@vinnielee.io>
2022-04-25 07:48:12 -0400

    initial

---
+++ variables.tf
@@ -0,0 +1,10 @@
+variable "region" {
+  description = "AWS region"
+  default     = "us-east-1"
+}
+
+variable "instance_type" {
+  description = "Type of EC2 instance to provision"
+  default     = "t2.micro"
+}
+
---
+++ versions.tf
@@ -0,0 +1,22 @@
+terraform {
+  cloud {
+    organization = "vinnielee-io"
+
+    workspaces {
+      name = "AWS-SSH-TF-Demo"
+    }
+  }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.43.0"
+    }
+
+    random = {
+      source  = "hashicorp/random"
+      version = "3.0.0"
+    }
+  }
+
+  required_version = ">= 0.14.0"
+}
